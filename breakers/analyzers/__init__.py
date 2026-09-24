45fbd2de9d91502a01835664c710476c916fad23
Chinosonico <67437920+Ch1405041C0@users.noreply.github.com>
2026-09-24 20:27:44 -0300

    SCAN: expose requirements analyzer

--- breakers/analyzers/__init__.py
+++ breakers/analyzers/__init__.py
@@ -1,3 +1,4 @@
 from .registry import analyzers_for
+from .requirements import analyze_requirements_file, analyze_requirements_text
 
-__all__ = ["analyzers_for"]
+__all__ = ["analyzers_for", "analyze_requirements_file", "analyze_requirements_text"]
